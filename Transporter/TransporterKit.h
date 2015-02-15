021c90c84402685fc930c4108ec5066cfbf6d004
Phillip <phill@3sidedcube.com>
2015-02-15 22:45:52 +0000

    Edits info.plist, points api to non local host one

--- Transporter/TransporterKit.h
+++ Transporter/TransporterKit.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Phillip Caudell. All rights reserved.
 //
 
-static NSString * const TPAPIBaseAddress = @"http://localhost:1447/v1";
+static NSString * const TPAPIBaseAddress = @"http://api.transporterapp.co.uk/v1";
 
 #import "AppController.h"
 #import "DepartureController.h"
